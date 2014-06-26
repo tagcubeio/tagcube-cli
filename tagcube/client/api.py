4997a72f29af61715f45ed1f6f4f2ae111e847e1
Andres Riancho <andres.riancho@gmail.com>
2014-06-26 13:21:25 -0300

    Implemented test_auth_credentials

--- tagcube/client/api.py
+++ tagcube/client/api.py
@@ -6,6 +6,8 @@ import json
 class TagCubeClient(object):
 
     ROOT_URL = 'https://www.tagcube.io/'
+    API_VERSION = '1.0'
+    SELF_URL = '/users/~'
 
     def __init__(self, email, api_key):
         self.email = email
@@ -14,7 +16,11 @@ class TagCubeClient(object):
         self.configure_requests()
 
     def test_auth_credentials(self):
-        raise NotImplementedError
+        """
+        :return: True when the credentials are properly configured.
+        """
+        code, json = self.send_request(self.build_url(self.SELF_URL))
+        return code == requests.codes.ok
 
     def configure_requests(self):
         # We disable the logging of the requests module to avoid some infinite
@@ -38,3 +44,6 @@ class TagCubeClient(object):
             raise ValueError('Invalid HTTP method: "%s"' % method)
 
         return response.status_code, response.json()
+
+    def build_url(self, last_part):
+        return '%s%s%s' % (self.ROOT_URL, self.API_VERSION, last_part)
