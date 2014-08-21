d0ea1f87e17572b32ff46576153ecf120b7c7ab1
Andres Riancho <andres.riancho@gmail.com>
2014-08-21 16:27:34 -0300

    Finally decided to manually increase the version in order to force a push to pypi, this allows me to re-build without any issues Continuous delivery to pypi #4

--- ci/pypi-deploy.sh
+++ ci/pypi-deploy.sh
@@ -6,4 +6,17 @@ echo "[server-login]" > ~/.pypirc
 echo "username:" $PYPI_USER >> ~/.pypirc
 echo "password:" $PYPI_PASSWORD >> ~/.pypirc
 
-python setup.py sdist upload
+PACKAGE_URL='https://pypi.python.org/packages/source/t/tagcube-cli/'
+
+packages=`curl -f -s -S -k $PACKAGE_URL`
+tagcube_version=`python -c 'from tagcube import __VERSION__;print __VERSION__,'`
+current_package="tagcube-cli-$tagcube_version.tar.gz"
+
+if [[ $packages == *$current_package* ]]
+then
+    echo "Current package version is already at PyPi. If your intention was to"
+    echo "release a new version, you'll have to increase the version number."
+else
+    echo "Uploading $tagcube_version version to PyPi"
+    python setup.py sdist upload
+fi
