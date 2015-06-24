64cb54108e92f744bd408c2e6f8563c67147f150
Andres Riancho <andres.riancho@gmail.com>
2015-06-23 21:48:29 -0300

    Fix based on what I've learnt from w3af-api-client

--- ci/pypi-deploy.sh
+++ ci/pypi-deploy.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-set -e
-
 echo "[server-login]" > ~/.pypirc
 echo "username:" $PYPI_USER >> ~/.pypirc
 echo "password:" $PYPI_PASSWORD >> ~/.pypirc
 
-PACKAGE_URL='https://pypi.python.org/packages/source/t/tagcube-cli/'
+version=`python -c 'from tagcube import __VERSION__;print __VERSION__,'`
+curl -f -s -S -k -X GET -I https://pypi.python.org/packages/source/t/tagcube-cli/tagcube-cli-$version.tar.gz
 
-packages=`curl -f -s -S -k $PACKAGE_URL`
-tagcube_version=`python -c 'from tagcube import __VERSION__;print __VERSION__,'`
-current_package="tagcube-cli-$tagcube_version.tar.gz"
-
-if [[ $packages == *$current_package* ]]
+if [[ $? -eq 0 ]]
 then
     echo "Current package version is already at PyPi. If your intention was to"
     echo "release a new version, you'll have to increase the version number."
 else
-    echo "Uploading $tagcube_version version to PyPi"
+    echo "Uploading $version version to PyPi"
     python setup.py sdist upload
 fi
+
+exit 0
