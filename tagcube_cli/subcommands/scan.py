4e1fe1e431ddf797815d7fc7c5de794ecb297607
Andres Riancho <andres.riancho@gmail.com>
2015-07-08 15:51:51 -0300

    Fix command line argument name change

--- tagcube_cli/subcommands/scan.py
+++ tagcube_cli/subcommands/scan.py
@@ -8,10 +8,10 @@ def do_scan_start(client, cmd_args):
     cli_logger.debug('Authentication credentials are valid')
     cli_logger.debug('Starting web application scan')
 
-    scan_resource = client.quick_scan(cmd_args.url,
+    scan_resource = client.quick_scan(cmd_args.root_url,
                                       email_notify=cmd_args.email_notify,
                                       scan_profile=cmd_args.scan_profile,
-                                      path_list=cmd_args.path_list)
+                                      path_list=cmd_args.path_file)
 
     # pylint: disable=E1101
     cli_logger.info('Launched scan with id #%s' % scan_resource.id)
