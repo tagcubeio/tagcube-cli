1fa9610b1838cb4b2913923784e3605a77f36822
Andres Riancho <andres.riancho@gmail.com>
2015-07-08 12:45:58 -0300

    Shortcut

--- setup.py
+++ setup.py
@@ -45,7 +45,8 @@ setup(
 
     entry_points={
         'console_scripts':
-            ['tagcube-cli = tagcube_cli.main:main']
+            ['tagcube-cli = tagcube_cli.main:main',
+             'tagcube = tagcube_cli.main:main']
     },
 
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
